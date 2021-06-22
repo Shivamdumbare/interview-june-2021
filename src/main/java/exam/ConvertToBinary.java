9f809204cb482cd303fc986deeac312b3625b972
Shivamdumbare <86242001+Shivamdumbare@users.noreply.github.com>
2021-06-22 12:45:03 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,27 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    try
+    {
+      int n = integer.parselnt(br.readLine());
+      if(n<0)
+        throw new MyException("Number is negative doesn't support binary");
+      else
+      {
+        for (int i=31; i>=0; i--)
+        {
+          int k=n>>i;
+          if(k & 1)
+            cout<<"1";
+          else
+            cout<<"0";
+        }
+      }
+      catch(MyException m )
+      {
+        System.out.printn(m);
+      }
+          return "";
   }
+}
 }
